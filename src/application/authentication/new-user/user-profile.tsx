22d01778a134bc59176305011c21cae342a7f45b
Satish Mahajan <satishmahajan29@gmail.com>
2019-03-15 21:32:31 +0530

    resolved conflicts

--- src/application/authentication/new-user/user-profile.tsx
+++ src/application/authentication/new-user/user-profile.tsx
@@ -7,38 +7,33 @@ export default class UserProfile extends Component {
       email: '',
       password: ''
    }
-   handleEmail = (text) => {
-      this.setState({ email: text })
-   }
-   handlePassword = (text) => {
-      this.setState({ password: text })
-   }
-   login = (email, pass) => {
-      alert('email: ' + email + ' password: ' + pass)
-   }
+   handleEmail = (text: string) => this.setState({ email: text });
+   handlePassword = (text: string) => this.setState({ password: text });
+   login = (email: string, pass: string) => alert('email: ' + email + ' password: ' + pass);
+
    render() {
       return (
-         <View style = {styles.container}>
-            <TextInput style = {styles.input}
-               underlineColorAndroid = "transparent"
-               placeholder = "Email"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               onChangeText = {this.handleEmail}/>
-            
-            <TextInput style = {styles.input}
-               underlineColorAndroid = "transparent"
-               placeholder = "Password"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               onChangeText = {this.handlePassword}/>
-            
+         <View style={styles.container}>
+            <TextInput style={styles.input}
+               underlineColorAndroid="transparent"
+               placeholder="Email"
+               placeholderTextColor="#9a73ef"
+               autoCapitalize="none"
+               onChangeText={this.handleEmail} />
+
+            <TextInput style={styles.input}
+               underlineColorAndroid="transparent"
+               placeholder="Password"
+               placeholderTextColor="#9a73ef"
+               autoCapitalize="none"
+               onChangeText={this.handlePassword} />
+
             <TouchableOpacity
-               style = {styles.submitButton}
-               onPress = {
+               style={styles.submitButton}
+               onPress={
                   () => this.login(this.state.email, this.state.password)
                }>
-               <Text style = {styles.submitButtonText}> Submit </Text>
+               <Text style={styles.submitButtonText}> View Both </Text>
             </TouchableOpacity>
          </View>
       )
