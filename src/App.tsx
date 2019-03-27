e0b392cf25e73a03605259a05b008b059e5e7af9
Mahesh Katre <mahesh.katre@team.telstra.com>
2019-03-27 09:58:16 +0530

    added splash screen component using lottie lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import NumberSignIn from './application/authentication/new-user/number-sign-in';
-import UserProfile from './application/authentication/new-user/user-profile';
+// import NumberSignIn from './application/authentication/new-user/number-sign-in';
+// import UserProfile from './application/authentication/new-user/user-profile';
+import BasicExample from './application/components/splash-screen/index';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <NumberSignIn />
-        <UserProfile />
+        <BasicExample/>
+        {/* <NumberSignIn />
+        <UserProfile /> */}
       </View>
     );
   }
